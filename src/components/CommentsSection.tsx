fdb911075fcc421b7221c5f9e60ba3860b7d77b6
McKay <mmower777@gmail.com>
2023-07-24 22:45:44 -0600

    post comment started

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,9 +1,60 @@
-import {FC} from 'react'
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import PostComment from "./PostComment";
 
 interface CommentsSectionProps {
-    postId: string
+  postId: string;
 }
 
-const CommentsSection: FC<CommentsSectionProps> = ({postId}) => {
-    return <div>CommentsSection</div>
-}
+const CommentsSection = async ({ postId }: CommentsSectionProps) => {
+  const session = await getAuthSession();
+  const comments = await db.comment.findMany({
+    where: {
+      postId: postId,
+      replyToId: null,
+    },
+    include: {
+      author: true,
+      votes: true,
+      replies: {
+        include: {
+          author: true,
+          votes: true,
+        },
+      },
+    },
+  });
+  return (
+    <div className="flex flex-row gap-y-4 mt-4">
+      <hr className="w-full h-px py-4" />
+      {/** create a comment */}
+      <div className="flex flex-col gap-y-6 mt-4">
+        {comments
+          .filter((comment) => !comment.replyToId)
+          .map((topLevelComment) => {
+            const topLevelCommentVotesAmount = topLevelComment.votes.reduce(
+              (acc, vote) => {
+                if (vote.type === "UP") return acc + 1;
+                if (vote.type === "DOWN") return acc - 1;
+                return acc;
+              },
+              0
+            );
+
+            const topLevelCommentVote = topLevelComment.votes.find(
+              (vote) => vote.userId === session?.user.id
+            );
+
+
+            return <div key={topLevelComment.id} className="flex flex-col">
+                <div className="mb-2">
+                    <PostComment comment={topLevelComment} />
+                </div>
+            </div>;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default CommentsSection;
