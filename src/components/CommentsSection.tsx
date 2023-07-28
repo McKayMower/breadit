e6d1061f37a6a33f872afb8adcb842dcc237d67c
McKay <mmower777@gmail.com>
2023-07-27 22:38:54 -0600

    Comment bug fixed

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,22 +1,38 @@
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { Comment, CommentVote, User } from "@prisma/client";
 import PostComment from "./PostComment";
+import CreateComment from "./CreateComment";
+
+type ExtendedComment = Comment & {
+  votes: CommentVote[];
+  author: User;
+  replies: ReplyComment[];
+};
+
+type ReplyComment = Comment & {
+  votes: CommentVote[];
+  author: User;
+};
 
 interface CommentsSectionProps {
   postId: string;
+  comments: ExtendedComment[];
 }
 
 const CommentsSection = async ({ postId }: CommentsSectionProps) => {
   const session = await getAuthSession();
+
   const comments = await db.comment.findMany({
     where: {
       postId: postId,
-      replyToId: null,
+      replyToId: null, // only fetch top-level comments
     },
     include: {
       author: true,
       votes: true,
       replies: {
+        // first level replies
         include: {
           author: true,
           votes: true,
@@ -24,15 +40,18 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
       },
     },
   });
+
   return (
-    <div className="flex flex-row gap-y-4 mt-4">
-      <hr className="w-full h-px py-4" />
-      {/** create a comment */}
+    <div className="flex flex-col gap-y-4 mt-4">
+      <hr className="w-full h-px my-6" />
+
+      <CreateComment postId={postId} />
+
       <div className="flex flex-col gap-y-6 mt-4">
         {comments
           .filter((comment) => !comment.replyToId)
           .map((topLevelComment) => {
-            const topLevelCommentVotesAmount = topLevelComment.votes.reduce(
+            const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
               (acc, vote) => {
                 if (vote.type === "UP") return acc + 1;
                 if (vote.type === "DOWN") return acc - 1;
@@ -45,12 +64,47 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
               (vote) => vote.userId === session?.user.id
             );
 
-
-            return <div key={topLevelComment.id} className="flex flex-col">
+            return (
+              <div key={topLevelComment.id} className="flex flex-col">
                 <div className="mb-2">
-                    <PostComment comment={topLevelComment} />
+                  <PostComment
+                    comment={topLevelComment}
+                    currentVote={topLevelCommentVote}
+                    votesAmt={topLevelCommentVotesAmt}
+                    postId={postId}
+                  />
                 </div>
-            </div>;
+
+                {/* Render replies */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length) // Sort replies by most liked
+                  .map((reply) => {
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === "UP") return acc + 1;
+                      if (vote.type === "DOWN") return acc - 1;
+                      return acc;
+                    }, 0);
+
+                    const replyVote = reply.votes.find(
+                      (vote) => vote.userId === session?.user.id
+                    );
+
+                    return (
+                      <div
+                        key={reply.id}
+                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    );
+                  })}
+              </div>
+            );
           })}
       </div>
     </div>
