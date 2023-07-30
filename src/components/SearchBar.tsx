f2a618db41b2aef45fdfc84884d166900b48296d
McKay <mmower777@gmail.com>
2023-07-29 23:44:32 -0600

    searchbar with debouncing

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,37 +1,105 @@
-"use client";
-import { FC, useState } from "react";
-import { Command, CommandInput } from "./ui/Command";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Prisma, Subreddit } from "@prisma/client";
+'use client'
+
+import { Prisma, Subreddit } from '@prisma/client'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import debounce from 'lodash.debounce'
+import { usePathname, useRouter } from 'next/navigation'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/Command'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
+import { Users } from 'lucide-react'
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
-  const [input, setInput] = useState<string>("");
+  const [input, setInput] = useState<string>('')
+  const pathname = usePathname()
+  const commandRef = useRef<HTMLDivElement>(null)
+  const router = useRouter()
 
-  const {data: queryResults, refetch, isFetched, isFetching} = useQuery({
+  useOnClickOutside(commandRef, () => {
+    setInput('')
+  })
+
+  const request = debounce(async () => {
+    refetch()
+  }, 300)
+
+  const debounceRequest = useCallback(() => {
+    request()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const {
+    isFetching,
+    data: queryResults,
+    refetch,
+    isFetched,
+  } = useQuery({
     queryFn: async () => {
-      if(!input) return []
-      const {data} = await axios.get(`/api/search?q=${input}`)
+      if (!input) return []
+      const { data } = await axios.get(`/api/search?q=${input}`)
       return data as (Subreddit & {
         _count: Prisma.SubredditCountOutputType
-      })
+      })[]
     },
-    queryKey: ["search-query"],
-    enabled: false, // fetches when we type, not when the component renders.
-  });
+    queryKey: ['search-query'],
+    enabled: false,
+  })
+
+  useEffect(() => {
+    setInput('')
+  }, [pathname])
 
   return (
-    <Command className="relative rounded-lg border max-w-lg z-50 overflow-visible">
+    <Command
+      ref={commandRef}
+      className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
       <CommandInput
+        
+        // isLoading={isFetching}
+        onValueChange={(text) => {
+          setInput(text)
+          debounceRequest()
+        }}
         value={input}
-        onValueChange={(text) => setInput(text)}
-        placeholder="Search communities..."
-        className="outline-none border-none focus:border-none focus:outline-none ring-0"
+        className='outline-none border-none focus:border-none focus:outline-none ring-0'
+        placeholder='Search communities...'
       />
-    </Command>
-  );
-};
 
-export default SearchBar;
+      {input.length > 0 && (
+        <CommandList className='absolute bg-white top-full inset-x-0 shadow rounded-b-md'>
+          {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
+          {(queryResults?.length ?? 0) > 0 ? (
+            <CommandGroup heading='Communities'>
+              {queryResults?.map((subreddit) => (
+                <CommandItem
+                  onSelect={(e) => {
+                    router.push(`/r/${e}`)
+                    router.refresh()
+                  }}
+                  key={subreddit.id}
+                  value={subreddit.name}>
+                  <Users className='mr-2 h-4 w-4' />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
+        </CommandList>
+      )}
+    </Command>
+  )
+}
+
+export default SearchBar
