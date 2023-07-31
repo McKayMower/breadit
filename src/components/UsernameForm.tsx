8b99e200265b0c2ee96ca63417f1b279b3d44a5c
McKay <mmower777@gmail.com>
2023-07-30 20:06:21 -0600

    change username api

--- src/components/UsernameForm.tsx
+++ src/components/UsernameForm.tsx
@@ -16,6 +16,10 @@ import {
 import { Label } from "./ui/Label";
 import { Input } from "./ui/Input";
 import { Button } from "./ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 interface UsernameFormProps {
   user: Pick<User, "id" | "username">;
@@ -33,8 +37,46 @@ const UsernameForm: FC<UsernameFormProps> = ({ user }) => {
     },
   });
 
+  const router = useRouter();
+
+  const { mutate: updateUsername, isLoading } = useMutation({
+    mutationFn: async ({ name }: UsernameRequest) => {
+      const payload: UsernameRequest = { name };
+
+      const { data } = await axios.patch(`/api/username`, payload);
+      return data;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409)
+          return toast({
+            title: "Username already taken.",
+            description: "Please choose a different Username",
+            variant: "destructive",
+          });
+      }
+
+      return toast({
+        title: "There was an error.",
+        description: "Could not change username.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        description: "Your username has been updated.",
+      });
+
+      router.refresh();
+    },
+  });
+
   return (
-    <form onSubmit={handleSubmit(() => {})}>
+    <form
+      onSubmit={handleSubmit((e) => {
+        updateUsername(e);
+      })}
+    >
       <Card>
         <CardHeader>
           <CardTitle>Your username</CardTitle>
@@ -63,7 +105,9 @@ const UsernameForm: FC<UsernameFormProps> = ({ user }) => {
           )}
         </CardContent>
         <CardFooter>
-          <Button>Change Name</Button>
+          <Button isLoading={isLoading} type="submit">
+            Change Name
+          </Button>
         </CardFooter>
       </Card>
     </form>
